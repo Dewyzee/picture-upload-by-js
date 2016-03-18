c2ce5b270557787cdcc6706e8c054f54a0e827fe
xutao <413913697@qq.com>
2016-03-18 15:14:38 +0800

    Update server.php

--- php_server/server.php
+++ php_server/server.php
@@ -7,12 +7,6 @@ if ($_FILES["file"]["error"] > 0)
 else
   {
   $output = array('size'=>($_FILES["file"]["size"] / 1024), 'type'=>$_FILES["file"]["type"], 'name'=>$_FILES["file"]["name"]);
-  $json_output = json_encode($output);
-  echo '
-    {
-      "errCode": 0,
-      "data": $json_output
-    }
-  '
+  echo json_encode($output)
   }
 ?>
