53c54b97e2ff7cb9668f1edf8b89ac744d09c8bf
xutao <413913697@qq.com>
2016-03-18 15:20:33 +0800

    Update server.php

--- php_server/server.php
+++ php_server/server.php
@@ -7,6 +7,6 @@ if ($_FILES["file"]["error"] > 0)
 else
   {
   $output = array('size'=>($_FILES["file"]["size"] / 1024), 'type'=>$_FILES["file"]["type"], 'name'=>$_FILES["file"]["name"]);
-  echo json_encode($output)
+  echo json_encode($output);
   }
 ?>
