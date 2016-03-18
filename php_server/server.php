a401e13b013147d77717aaa49111423c2c51aead
xutao <413913697@qq.com>
2016-03-18 15:11:39 +0800

    Update server.php

--- php_server/server.php
+++ php_server/server.php
@@ -6,14 +6,12 @@ if ($_FILES["file"]["error"] > 0)
   }
 else
   {
+  $output = array('size'=>($_FILES["file"]["size"] / 1024), 'type'=>$_FILES["file"]["type"], 'name'=>$_FILES["file"]["name"]);
+  $json_output = json_encode($output);
   echo '
     {
       "errCode": 0,
-      "data": {
-        "filename": $_FILES["file"]["name"],
-        "filesize": ($_FILES["file"]["size"] / 1024),
-        "filetype": $_FILES["file"]["type"]
-      }
+      "data": $json_output
     }
   '
   }
